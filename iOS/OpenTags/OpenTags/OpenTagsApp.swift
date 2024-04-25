ec0b27a2374bc83453a78babd1cf1fc03948f2a6
JadAriss <jadariss1@gmail.com>
2024-04-25 15:12:56 -0700

    Add default proj w/o git

---
+++ iOS/OpenTags/OpenTags/OpenTagsApp.swift
@@ -0,0 +1,17 @@
+//
+//  OpenTagsApp.swift
+//  OpenTags
+//
+//  Created by Jad Ariss on 4/25/24.
+//
+
+import SwiftUI
+
+@main
+struct OpenTagsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
