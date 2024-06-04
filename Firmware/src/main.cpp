ad898f67626aed2f114901824a9b251c50661908
mohakvni <mohakvaswani@gmail.com>
2024-06-04 00:40:35 -0700

    Clean up

--- Firmware/src/main.cpp
+++ Firmware/src/main.cpp
@@ -10,13 +10,24 @@ extern "C" {
 #include <BLECharacteristic.h>
 #include <bluefruit.h>
 
-#define APP_NAME "READ DEV ID\r\n"
+
+// Definitions 
+#define APP_NAME "OPENTAG"
 #define SPI_MOSI 10
 #define SPI_MISO 9
 #define SPI_SCK 8
 #define SPI_CS 0
 #define RST 1
 #define IRQ 2
+
+// BLE Service
+BLEService myService("26b886e0-0bab-4d96-a8f3-f44fe442a700");
+BLEService ANIService("48fe3e40-0817-4bb2-8633-3073689c2dba");
+
+// BLE Characteristics
+BLECharacteristic myTxChar("6E400002-B5A3-F393-E0A9-E50E24DCCA9E", BLEWrite | BLENotify | BLEIndicate, 38, true);
+BLECharacteristic myRxChar("6E400003-B5A3-F393-E0A9-E50E24DCCA9E", BLERead);
+BLECharacteristic ACD("95e8d9d5-d8ef-4721-9a4e-807375f53328", BLERead);
 
 #define TX_ANT_DLY 16385
 #define RX_ANT_DLY 16385
@@ -72,15 +83,9 @@ volatile static uint8_t start_uwb = 0;
 
 dwt_config_t * dwt_config;
 
-
 /* Hold copies of computed time of flight and distance here for reference so that it can be examined at a debug breakpoint. */
 static double tof;
 static double distance;
-
-
-// BLE Service
-BLEService myService("26b886e0-0bab-4d96-a8f3-f44fe442a700");
-BLEService ANIService("48fe3e40-0817-4bb2-8633-3073689c2dba");
 
 static uint8_t accessory_config_data_buffer[sizeof(struct AccessoryConfigurationData)];
 static uint8_t accessory_config_data_length;
@@ -88,13 +93,15 @@ static uint8_t accessory_config_data_length;
 fira_device_configure_t fira_config;
 extern dwt_txconfig_t txconfig_options;
 
-// BLE Characteristic for Tx
-BLECharacteristic myTxChar("6E400002-B5A3-F393-E0A9-E50E24DCCA9E", BLEWrite | BLENotify | BLEIndicate, 38, true);
-BLECharacteristic myRxChar("6E400003-B5A3-F393-E0A9-E50E24DCCA9E", BLERead);
+static void send_accessory_config_data();
 
-BLECharacteristic ACD("95e8d9d5-d8ef-4721-9a4e-807375f53328", BLERead);
+//**************************************************************************************************************
+// BLE Related Methods
+//*******************************************************************************************************
 
-
+/**
+ * Method to notify the client from the peripheral
+*/
 static void send_ble_data(uint8_t * buffer, uint16_t data_len){
   bool success = myTxChar.notify(buffer, data_len);
   if (success) {
@@ -104,127 +111,20 @@ static void send_ble_data(uint8_t * buffer, uint16_t data_len){
   }
 }
 
-void print_buffer_binary(uint8_t* buffer, size_t length) {
-    for (size_t i = 0; i < length; ++i) {
-        for (int8_t j = 7; j >= 0; --j) {
-            Serial.print((buffer[i] >> j) & 1);
-        }
-        Serial.print(" ");
-    }
-    Serial.println();
-}
-
-void print_binary(uint32_t value) {
-    char binary_str[33]; // 32 bits + null terminator
-    binary_str[32] = '\0'; // Null terminator
-
-    for (int i = 31; i >= 0; --i) {
-        binary_str[i] = (value & 1) ? '1' : '0';
-        value >>= 1;
-    }
-
-    Serial.println(binary_str);
-}
-
-static void send_accessory_config_data() {
-
-    Serial.println("Sending Config data for the first time");
-    niq_reinit();
-
-    // The Accessory Configuration Data in intended to be constructed by the embedded application.
-
-    // The message structure is the following:
-    // ------------------------
-    //     majorVersion         -- the major version from section 3.3 of the Nearby Interaction Accessory Protocol Specification, Developer Preview, Release 1.
-    //     minorVersion         -- the minor version from section 3.3 of the Nearby Interaction Accessory Protocol Specification, Developer Preview, Release 1.
-    //     preferredUpdateRate  -- a selection of one of the options from table 3-3 in the Nearby Interaction Accessory Protocol Specification, Developer Preview, Release 1.
-    //     rfu[10]              -- reserved for future use.
-    //     uwbConfigDataLength  -- the length of the UWB config data as provided by the UWB middleware.
-    //     uwbConfigData        -- the UWB config data as provided by the UWB middleware, according to section 4.4.2 on the UWB Interoperability Specification, Developer Preview, Release 1.
-    // ------------------------
-    //
-    // In order to populate `uwbConfigData` and `uwbConfigDataLength`, the embedded appliaction needs to query
-    // the UWB middleware which is compliant with the UWB Interoperability Specification.
-    //
-    // Once the Accessory Configuration Data is constucted and populated, the embedded application needs to send it to the iOS app.
-
-    uint8_t buffer[sizeof(struct AccessoryConfigurationData)+1]; // + 1 for the MessageId
-    memset(buffer, 0, sizeof(struct AccessoryConfigurationData)+1);
-
-    ni_packet_t * packet = (ni_packet_t *)buffer;
-    packet->message_id = (uint8_t)MessageId_accessoryConfigurationData;
-
-    // Embeded appliaction responsibility
-    struct AccessoryConfigurationData *config = (struct AccessoryConfigurationData *)packet->payload;
-    config->majorVersion = NI_ACCESSORY_PROTOCOL_SPEC_MAJOR_VERSION;
-    config->minorVersion = NI_ACCESSORY_PROTOCOL_SPEC_MINOR_VERSION;
-    config->preferredUpdateRate = PreferredUpdateRate_Automatic;
-    
-    // UWB middleware responsibility
-    niq_populate_accessory_uwb_config_data(&config->uwbConfigData, &config->uwbConfigDataLength);
-
-    send_ble_data(buffer, (uint16_t)(config->uwbConfigDataLength + ACCESSORY_CONFIGURATION_DATA_FIX_LEN + 1));
-    memset(buffer, 0, sizeof(struct AccessoryConfigurationData)+1);
-}
-
-void spi_rd_err_cb(){
-  Serial.print("Bruhhh");
-}
-
+/**
+ * Connect Callback for BLE
+*/
 static void send_ack_uwb_started(void)
 {
       // Send "UWB did start" BLE message to the app
-      Serial.println("Sending Acknowledgement");
       uint8_t buffer[1] = {2};
       send_ble_data(buffer, 1);
 }
 
-
-/*
- * @brief callback used for niq lib to signal session start
- */
-void ResumeUwbTasks(void)
-{ 
-    start_uwb = 1;
-}
-
-
-/*
- * @brief callback used for niq lib to signal session stop
-*/
-void StopUwbTask(void)
-{
-    start_uwb = 0;
-}
-
-// Wrapper for nrf_crypto_init
-void nrf_crypto_init_wrapper(void) {
-    bool result = nRFCrypto.begin();
-    if (!result) {
-        Serial.println("Crypto init failed with error");
-    }
-}
-
-// Wrapper for nrf_crypto_uninit
-void nrf_crypto_uninit_wrapper(void) {
-  nRFCrypto.end();
-}
-
-void nrf_crypto_rng_vector_generate_wrapper(uint8_t * const p_target, size_t size){
-  bool result = nRFCrypto.Random.generate(p_target, size);
-    if (!result) {
-        Serial.println("Crypto generate failed with error");
-    }
-}
-
-//**************************************************************************************************************
-// callback function
-//*******************************************************************************************************
 void connect_callback(uint16_t conn_handle)
 {
   Serial.print("【connect_callback】 conn_Handle : ");
   Serial.println(conn_handle, HEX);
-//  conn = conn_handle;
 
   // Get the reference to current connection
   BLEConnection* connection = Bluefruit.Connection(conn_handle);
@@ -245,13 +145,13 @@ void connect_callback(uint16_t conn_handle)
   // request mtu exchange 23 --> 247
   Serial.print("Request to change MTU : "); Serial.print(connection->getMtu());
   connection->requestMtuExchange(247);  // max 247
-//  connection->requestMtuExchange(123);  // max 247
+  //  connection->requestMtuExchange(123);  // max 247
   delayMicroseconds(1000000);  // delay a bit for all the request to complete
   Serial.print(" --> "); Serial.println(connection->getMtu());
 
   // request connection interval  16(20mS) --> 16(20mS)
   Serial.print("Request to change Interval : "); Serial.print(connection->getConnectionInterval());
-//  connection->requestConnectionParameter(16); // 20mS(in unit of 1.25) default 20mS
+  //  connection->requestConnectionParameter(16); // 20mS(in unit of 1.25) default 20mS
   delayMicroseconds(1000000);  // delay a bit for all the request to complete
   Serial.print(" --> "); Serial.println(connection->getConnectionInterval());
 
@@ -262,7 +162,9 @@ void connect_callback(uint16_t conn_handle)
   Serial.println(central_name);
 }
 
-//***********************************************************************************************
+/**
+ * Disconnect Callback for BLE
+*/
 void disconnect_callback(uint16_t conn_handle, uint8_t reason)
 {
   (void) conn_handle;
@@ -272,8 +174,10 @@ void disconnect_callback(uint16_t conn_handle, uint8_t reason)
   Serial.println(reason, HEX);
 }
 
-
-
+/**
+ * Method to send the iPhone acknowledgement of 
+ * receiving of Shared Configuration data
+*/
 static void send_ack_uwb_stopped(void)
 {
     uint8_t buffer[1];
@@ -282,6 +186,9 @@ static void send_ack_uwb_stopped(void)
     send_ble_data(buffer, 1);
 }
 
+/**
+ * This is the callback method passed onto the TX Characteristic 
+*/
 void onDataWritten(uint16_t conn_hdl, BLECharacteristic* chr, uint8_t* data, uint16_t len) {
   ni_packet_t * packet = (ni_packet_t *)data;
   switch (packet->message_id) {
@@ -321,6 +228,179 @@ void onDataWritten(uint16_t conn_hdl, BLECharacteristic* chr, uint8_t* data, uin
       break;
     }
   }
+}
+
+//**************************************************************************************************************
+// NIQ Middleware Related Methods
+//*******************************************************************************************************
+
+/*
+ * @brief callback used for niq lib to signal session start
+ */
+void ResumeUwbTasks(void)
+{ 
+    start_uwb = 1;
+}
+
+/*
+ * @brief callback used for niq lib to signal session stop
+*/
+void StopUwbTask(void)
+{
+    start_uwb = 0;
+}
+
+// Wrapper for nrf_crypto_init
+void nrf_crypto_init_wrapper(void) {
+    bool result = nRFCrypto.begin();
+    if (!result) {
+        Serial.println("Crypto init failed with error");
+    }
+}
+
+// Wrapper for nrf_crypto_uninit
+void nrf_crypto_uninit_wrapper(void) {
+  nRFCrypto.end();
+}
+
+void nrf_crypto_rng_vector_generate_wrapper(uint8_t * const p_target, size_t size){
+  bool result = nRFCrypto.Random.generate(p_target, size);
+    if (!result) {
+        Serial.println("Crypto generate failed with error");
+    }
+}
+
+
+/**
+ * Method to generate and send accesory config data
+ * Designed as per structure defined in the
+ * Nearby Interaction Accessory Protocol Specification - Release 2
+*/
+static void send_accessory_config_data() {
+    niq_reinit();
+
+    // The Accessory Configuration Data in intended to be constructed by the embedded application.
+
+    // The message structure is the following:
+    // ------------------------
+    //     majorVersion         -- the major version from section 3.3 of the Nearby Interaction Accessory Protocol Specification, Developer Preview, Release 1.
+    //     minorVersion         -- the minor version from section 3.3 of the Nearby Interaction Accessory Protocol Specification, Developer Preview, Release 1.
+    //     preferredUpdateRate  -- a selection of one of the options from table 3-3 in the Nearby Interaction Accessory Protocol Specification, Developer Preview, Release 1.
+    //     rfu[10]              -- reserved for future use.
+    //     uwbConfigDataLength  -- the length of the UWB config data as provided by the UWB middleware.
+    //     uwbConfigData        -- the UWB config data as provided by the UWB middleware, according to section 4.4.2 on the UWB Interoperability Specification, Developer Preview, Release 1.
+    // ------------------------
+    //
+    // In order to populate `uwbConfigData` and `uwbConfigDataLength`, the embedded appliaction needs to query
+    // the UWB middleware which is compliant with the UWB Interoperability Specification.
+    //
+    // Once the Accessory Configuration Data is constucted and populated, the embedded application needs to send it to the iOS app.
+
+    uint8_t buffer[sizeof(struct AccessoryConfigurationData)+1]; // + 1 for the MessageId
+    memset(buffer, 0, sizeof(struct AccessoryConfigurationData)+1);
+
+    ni_packet_t * packet = (ni_packet_t *)buffer;
+    packet->message_id = (uint8_t)MessageId_accessoryConfigurationData;
+
+    // Embeded appliaction responsibility
+    struct AccessoryConfigurationData *config = (struct AccessoryConfigurationData *)packet->payload;
+    config->majorVersion = NI_ACCESSORY_PROTOCOL_SPEC_MAJOR_VERSION;
+    config->minorVersion = NI_ACCESSORY_PROTOCOL_SPEC_MINOR_VERSION;
+    config->preferredUpdateRate = PreferredUpdateRate_Automatic;
+    
+    // UWB middleware responsibility
+    niq_populate_accessory_uwb_config_data(&config->uwbConfigData, &config->uwbConfigDataLength);
+
+    send_ble_data(buffer, (uint16_t)(config->uwbConfigDataLength + ACCESSORY_CONFIGURATION_DATA_FIX_LEN + 1));
+    memset(buffer, 0, sizeof(struct AccessoryConfigurationData)+1);
+}
+
+//**************************************************************************************************************
+// Debug Methods
+//*******************************************************************************************************
+
+/**
+ * Method to print out the buffer contents in binary
+*/
+void print_buffer_binary(uint8_t* buffer, size_t length) {
+    for (size_t i = 0; i < length; ++i) {
+        for (int8_t j = 7; j >= 0; --j) {
+            Serial.print((buffer[i] >> j) & 1);
+        }
+        Serial.print(" ");
+    }
+    Serial.println();
+}
+
+/**
+ * Method to print our value in binary
+*/
+void print_binary(uint32_t value) {
+    char binary_str[33]; // 32 bits + null terminator
+    binary_str[32] = '\0'; // Null terminator
+
+    for (int i = 31; i >= 0; --i) {
+        binary_str[i] = (value & 1) ? '1' : '0';
+        value >>= 1;
+    }
+
+    Serial.println(binary_str);
+}
+
+/**
+ * Method to debug an niq packet and its content
+*/
+
+void debug(ni_packet_t * packet){
+
+    struct AccessoryConfigurationData *config = (struct AccessoryConfigurationData *)packet->payload;
+    Serial.println((uint8_t)config->uwbConfigDataLength);
+
+    Serial.println("Buffer in binary format (little-endian):");
+
+    // Print the message ID
+    Serial.println("Message ID");
+    print_buffer_binary(&packet->message_id, 1);
+
+    // Print the rest of the buffer in little-endian format
+    Serial.println("Major Version");
+    uint8_t* data_ptr = (uint8_t*)&config->majorVersion;
+    for (int i = 0; i < sizeof(config->majorVersion); ++i) {
+        print_buffer_binary(&data_ptr[i], 1);
+    }
+
+    Serial.println("Minor Version");
+    data_ptr = (uint8_t*)&config->minorVersion;
+    for (int i = 0; i < sizeof(config->minorVersion); ++i) {
+        print_buffer_binary(&data_ptr[i], 1);
+    }
+
+
+    Serial.println("PUR");
+    data_ptr = &config->preferredUpdateRate;
+    print_buffer_binary(data_ptr, 1);
+
+    for (int i = 0; i < sizeof(config->rfu); ++i) {
+        print_buffer_binary(&config->rfu[i], 1);
+    }
+
+    Serial.println("uwbConfigDataLength");
+    data_ptr = &config->uwbConfigDataLength;
+    print_buffer_binary(data_ptr, 1);
+
+    Serial.println("uwbConfigData");
+    for (int i = 0; i < config->uwbConfigDataLength; ++i) {
+        print_buffer_binary(&config->uwbConfigData[i], 1);
+    }
+}
+
+
+//**************************************************************************************************************
+// Setup and Looping
+//*******************************************************************************************************
+
+void spi_rd_err_cb(){
+  Serial.print("SPI Read error Call Back");
 }
 
 void setup() {
@@ -439,60 +519,10 @@ void setup() {
   Bluefruit.Advertising.setInterval(32, 244);    // in unit of 0.625 ms
   Bluefruit.Advertising.setFastTimeout(30);      // number of seconds in fast mode
   Bluefruit.Advertising.start(0);                // 0 = Don't stop advertising after n seconds
-  Serial.println("Waiting for Connection");
+  Serial.println("Awaiting for Connection");
 }
 
 void loop() {
-
   while (!start_uwb){}
-  Serial.println("Exited out of volatile while loop");
-}
-
-void debug(){
-  //set_accessory_uwb_config_data(packet->payloxad);
-    Serial.println((uint8_t)config->uwbConfigDataLength);
-
-    Serial.println("Buffer in binary format (little-endian):");
-
-    // Print the message ID
-    Serial.println("Message ID");
-    print_buffer_binary(&packet->message_id, 1);
-
-    // Print the rest of the buffer in little-endian format
-    Serial.println("Major Version");
-    uint8_t* data_ptr = (uint8_t*)&config->majorVersion;
-    for (int i = 0; i < sizeof(config->majorVersion); ++i) {
-        print_buffer_binary(&data_ptr[i], 1);
-    }
-
-    Serial.println("Minor Version");
-    data_ptr = (uint8_t*)&config->minorVersion;
-    for (int i = 0; i < sizeof(config->minorVersion); ++i) {
-        print_buffer_binary(&data_ptr[i], 1);
-    }
-
-
-    Serial.println("PUR");
-    data_ptr = &config->preferredUpdateRate;
-    print_buffer_binary(data_ptr, 1);
-
-    for (int i = 0; i < sizeof(config->rfu); ++i) {
-        print_buffer_binary(&config->rfu[i], 1);
-    }
-
-    Serial.println("uwbConfigDataLength");
-    data_ptr = &config->uwbConfigDataLength;
-    print_buffer_binary(data_ptr, 1);
-
-    Serial.println("uwbConfigData");
-    for (int i = 0; i < config->uwbConfigDataLength; ++i) {
-        print_buffer_binary(&config->uwbConfigData[i], 1);
-    }
-    
-    if (accessory_config_data_length <= sizeof(accessory_config_data_buffer))
-    {
-      memcpy(accessory_config_data_buffer, packet -> payload, accessory_config_data_length);
-    }
-    ACD.setBuffer(accessory_config_data_buffer, (uint16_t)accessory_config_data_length);
-
+  //start_TWR(); --> To be implemented
 }
